b661be6e6b095aa9e72a22654dae56c0b3b27cc0
Samuel Sciolla <ssciolla@umich.edu>
2024-06-11 16:06:58 -0400

    Add initial fix and repo structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# aspace-trees-fix
+
+This ArchivesSpace plugin includes a small patch of an existing method in 
+[backend/app/model/mixin/trees.rb](https://github.com/archivesspace/archivesspace/blob/master/backend/app/model/mixins/trees.rb). The change is intended to resolve a bug,
+reported to ArchivesSpace as [issue 3232](https://github.com/archivesspace/archivesspace/issues/3232).
---
+++ backend/plugin_init.rb
@@ -0,0 +1,13 @@
+require "logger"
+require "json"
+
+Logger.new($stdout).info("Loading aspace-trees-fix plugin in backend")
+
+# Overrides current build_node_query method to ensure ordering is consistent between batches
+# See https://github.com/archivesspace/archivesspace/blob/704cd60416b643d562df9fe05af83955152df8fd/backend/app/model/mixins/trees.rb#L46-L48
+
+module Trees
+  def build_node_query
+    self.class.node_model.this_repo.filter(:root_record_id => self.id).order(:ref_id)
+  end
+end
